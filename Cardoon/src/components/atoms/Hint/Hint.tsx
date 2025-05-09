9cf77f64cb3c8ec2b377dfea414a95beb7760eaf
MaximePie <maxime.pie.mail@gmail.com>
2025-05-09 07:47:08 +0200

    refactor: use generated ID for tooltip instead of hard coded ones

--- Cardoon/src/components/atoms/Hint/Hint.tsx
+++ Cardoon/src/components/atoms/Hint/Hint.tsx
@@ -1,16 +1,19 @@
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import { Tooltip } from "react-tooltip";
+import { useId } from "react";
 interface HintProps {
   text: string;
   customClassName?: string;
 }
 export const Hint = ({ text, customClassName }: HintProps) => {
+  const id = useId();
+
   return (
     <span className={`Hint ${customClassName || ""}`}>
-      <Tooltip id="hint-tooltip" content={text} className="Hint__tooltip" />
+      <Tooltip id={id} content={text} className="Hint__tooltip" />
       <HelpOutlineIcon
         className="Hint__icon"
-        data-tooltip-id="hint-tooltip"
+        data-tooltip-id={id}
         data-tooltip-content={text}
       />
     </span>

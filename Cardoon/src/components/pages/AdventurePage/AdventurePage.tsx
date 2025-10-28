cfef7f394b7b5aa842e4bf39591e0a4e56fcb8e1
MaximePie <maxime.pie.mail@gmail.com>
2025-10-28 17:30:43 +0100

    feat: add basic adventure battle feature

--- Cardoon/src/components/pages/AdventurePage/AdventurePage.tsx
+++ Cardoon/src/components/pages/AdventurePage/AdventurePage.tsx
@@ -1,8 +1,12 @@
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import StarBorderPurple500Icon from "@mui/icons-material/StarBorderPurple500";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
 import { useEffect, useState } from "react";
 import enemy from "../../../assets/Enemies/NightBorne_idle.gif";
 import { useUser } from "../../../hooks/contexts/useUser";
 import { ACTIONS, usePut } from "../../../hooks/server";
 import { PopulatedUserCard, User } from "../../../types/common";
+import ExpBar from "../../atoms/ExpBar/ExpBar";
 import Card from "../../molecules/Card/Card";
 interface PutResult {
   user: User;
@@ -15,6 +19,7 @@ interface Enemy {
   currentHealth: number;
   attackDamage: number;
   defense: number;
+  experience: number; // Given exp when defeated
 }
 
 const enemies = [
@@ -24,27 +29,31 @@ const enemies = [
     currentHealth: 100,
     attackDamage: 15,
     defense: 5,
+    experience: 50,
   },
   // Ajoutez plus d'ennemis ici si nécessaire
 ];
 
-const hero = {
+interface Hero {
+  name: string;
+  maxHealth: number;
+  currentHealth: number;
+  attackDamage: number;
+  defense: number;
+  level: number;
+  experience: number;
+  experienceToNextLevel: number;
+}
+
+const baseHero = {
   name: "Hero",
   maxHealth: 120,
   currentHealth: 120,
   attackDamage: 20,
   defense: 10,
-  attack(enemy: Enemy, isCorrect: boolean) {
-    if (isCorrect) {
-      const heroDamange = Math.max(0, this.attackDamage - enemy.defense);
-      const enemyDamage = Math.max(0, enemy.attackDamage - this.defense);
-      enemy.currentHealth = Math.max(0, enemy.currentHealth - heroDamange);
-      this.currentHealth = Math.max(0, this.currentHealth - enemyDamage);
-    } else {
-      const damage = Math.max(0, enemy.attackDamage - this.defense);
-      this.currentHealth = Math.max(0, this.currentHealth - damage * 1.5);
-    }
-  },
+  level: 1,
+  experience: 0,
+  experienceToNextLevel: 100,
 };
 
 const AdventurePage = () => {
@@ -52,12 +61,34 @@ const AdventurePage = () => {
   const [cardsInHand, setCardsInHand] = useState<PopulatedUserCard[]>(
     reviewUserCards.slice(0, 5)
   );
+  const [hero, setHero] = useState<Hero>(baseHero);
 
-  const [currentEnemy] = useState<Enemy>(enemies[0]);
+  const [currentEnemy, setCurrentEnemy] = useState<Enemy>(enemies[0]);
 
   const { put: updateUserCard, data: updateCardResponse } = usePut<PutResult>(
     ACTIONS.UPDATE_INTERVAL
   );
+
+  const attack = (enemy: Enemy, isCorrect: boolean) => {
+    if (isCorrect) {
+      const heroDamange = Math.max(0, hero.attackDamage - enemy.defense);
+      const enemyDamage = Math.max(0, enemy.attackDamage - hero.defense);
+      setCurrentEnemy((prev) => ({
+        ...prev,
+        currentHealth: Math.max(0, prev.currentHealth - heroDamange),
+      }));
+      setHero((prev) => ({
+        ...prev,
+        currentHealth: Math.max(0, prev.currentHealth - enemyDamage),
+      }));
+    } else {
+      const damage = Math.max(0, enemy.attackDamage - hero.defense);
+      setHero((prev) => ({
+        ...prev,
+        currentHealth: Math.max(0, prev.currentHealth - damage * 1.5),
+      }));
+    }
+  };
 
   useEffect(() => {
     setCardsInHand(
@@ -67,54 +98,93 @@ const AdventurePage = () => {
     );
   }, [reviewUserCards, updateCardResponse]);
 
+  const onEnemyDefeated = () => {
+    alert("Vous avez vaincu l'ennemi !");
+    currentEnemy.currentHealth = currentEnemy.maxHealth;
+    hero.currentHealth = hero.maxHealth;
+  };
+
   const removeCard = (card: PopulatedUserCard, isCorrect: boolean) => {
-    hero.attack(currentEnemy, isCorrect);
+    attack(currentEnemy, isCorrect);
+    if (currentEnemy.currentHealth <= 0) {
+      onEnemyDefeated();
+    }
     updateUserCard(card._id, { isCorrectAnswer: isCorrect });
     setCardsInHand((prev) => prev.filter((c) => c._id !== card._id));
     getReviewUserCards();
   };
   return (
     <div>
-      <h1>Adventure Page</h1>
-      <p>
-        Bienvenue, valeureux aventurier ! Prête à devenir une héroïne ? Allons-y
-        !
-      </p>
-      <div className="AdventurePage__characters">
-        <div className="AdventurePage__background"></div>
-        <div className="AdventurePage__Hero">
-          <img src="https://picsum.photos/200" alt="Hero Avatar" />
-          <div className="AdventurePage__healthBar">
-            <div
-              className="AdventurePage__healthBar__fill"
-              style={{
-                width: `${(hero.currentHealth / hero.maxHealth) * 100}%`,
-              }}
-            ></div>
-          </div>
-        </div>
-        <div className="AdventurePage__Enemy">
-          <img src={enemy} alt="Idle Enemy" />
-          <div className="AdventurePage__healthBar">
-            <div
-              className="AdventurePage__healthBar__fill"
-              style={{
-                width: `${(currentEnemy.currentHealth / currentEnemy.maxHealth) * 100}%`,
-              }}
-            ></div>
-          </div>
+      <div className="AdventurePage__profile">
+        <p>
+          Bienvenue, valeureux aventurier ! Prête à devenir une héroïne ?
+          Allons-y !
+        </p>
+        <div className="AdventurePage__stats">
+          <FavoriteIcon color="error" fontSize="small" /> {hero.currentHealth} /{" "}
+          {hero.maxHealth} &nbsp; &nbsp;
+          {hero.attackDamage} <WhatshotIcon color="error" fontSize="small" />
+          <StarBorderPurple500Icon color="warning" fontSize="small" />
+          {hero.level}
+          <ExpBar
+            currentExp={hero.experience}
+            maxExp={hero.experienceToNextLevel}
+          />
         </div>
       </div>
-      <div className="AdventurePage__Cards">
-        {cardsInHand.map((userCard: PopulatedUserCard) => (
-          <Card
-            key={userCard._id}
-            card={userCard}
-            onUpdate={removeCard}
-            isFlashModeOn={false}
-            onEditClick={() => {}}
-          />
-        ))}
+      <div className="AdventurePage__body">
+        <div className="AdventurePage__characters">
+          <div className="AdventurePage__background"></div>
+          <div className="AdventurePage__Hero">
+            <img
+              src="https://picsum.photos/200"
+              alt="Hero Avatar"
+              className="AdventurePage__characterImage"
+            />
+            <p>{hero.name}</p>
+            <p>
+              HP: {hero.currentHealth} / {hero.maxHealth}
+            </p>
+            <div className="AdventurePage__healthBar">
+              <div
+                className="AdventurePage__healthBar__fill"
+                style={{
+                  width: `${(hero.currentHealth / hero.maxHealth) * 100}%`,
+                }}
+              ></div>
+            </div>
+          </div>
+          <div className="AdventurePage__Enemy">
+            <img
+              src={enemy}
+              alt="Idle Enemy"
+              className="AdventurePage__characterImage"
+            />
+            <p>{currentEnemy.name}</p>
+            <p>
+              HP: {currentEnemy.currentHealth} / {currentEnemy.maxHealth}
+            </p>
+            <div className="AdventurePage__healthBar">
+              <div
+                className="AdventurePage__healthBar__fill"
+                style={{
+                  width: `${(currentEnemy.currentHealth / currentEnemy.maxHealth) * 100}%`,
+                }}
+              ></div>
+            </div>
+          </div>
+        </div>
+        <div className="AdventurePage__Cards">
+          {cardsInHand.map((userCard: PopulatedUserCard) => (
+            <Card
+              key={userCard._id}
+              card={userCard}
+              onUpdate={removeCard}
+              isFlashModeOn={false}
+              onEditClick={() => {}}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
